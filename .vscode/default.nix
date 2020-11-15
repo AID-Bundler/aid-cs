4e14c201aaaf97e5952d257f8937953c19701c33
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-15 07:03:53 -0700

    Deleted nix file

--- .vscode/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d12178b1c4a6ef1232c8c677573ba9db204e66ff.tar.gz") {} }:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nodejs
-  ];
-}
