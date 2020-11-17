2b8c668dc2569c44ad9d73827b381574c10e9381
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-17 09:19:11 -0700

    Updated AID Bundler

--- src/inputModifier.js
+++
@@ -1,8 +0,0 @@
-const { commandChecker } = require('./command_handler')
-
-module.exports = function (text, state, info, worldEntries, history) {
-  const data = new AIDData(text, state, info, worldEntries, history)
-  commandChecker(data)
-
-  return data.finalizeOutput()
-}
--- src/outputModifier.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (text, state, info, worldEntries, history) {
-  const data = new AIDData(text, state, info, worldEntries, history)
-  return data.finalizeOutput()
-}
