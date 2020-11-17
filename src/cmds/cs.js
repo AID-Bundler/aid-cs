93e43a1f479e15e8cb1a7b5f3eacbf2b1951631f
TheDudeFromCI <thedudefromci@gmail.com>
2020-11-17 09:19:27 -0700

    Converted to new command format

--- src/cmds/cs.js
+++ src/cmds/cs.js
@@ -1,38 +1,38 @@
-const { getPlayerCS, CS_CONFIG } = require('../character_sheet')
+const { getPlayerCS } = require('../character_sheet')
+const { Command } = require('aid-bundler')
 
-module.exports = function (data) {
-  data.useAI = false
-  data.text = ''
+module.exports = function (options) {
+  function handler (data) {
+    const cs = getPlayerCS(data)
 
-  const cs = getPlayerCS(data)
-  printCS(cs)
-}
-
-function printCS(cs) {
-  data.message = CS_CONFIG.visual.header
-
-  let row = ''
-  let col = 0
-  for (const att in cs.attributes) {
-
+    data.useAI = false
+    data.text = options.header
+  
+    let row = ''
+    let col = 0
+    for (const att in cs.attributes) {
+  
+      if (col > 0) {
+        row += ' | '
+      }
+  
+      row += att.name + ':'
+      row += att.value.toString().padStart(options.numberPadding - (att.name.length + 1), ' ')
+  
+      col += 1
+      if (col === options.columns) {
+        data.text += '\n' + row
+        col = 0
+        row = ''
+      }
+    }
+  
     if (col > 0) {
-      row += ' | '
+      data.text += '\n' + row
     }
-
-    row += att.name + ':'
-    row += att.value().toString().padStart(CS_CONFIG.visual.numberPadding, ' ')
-
-    col += 1
-    if (col === CS_CONFIG.visual.columns) {
-      data.message += '\n' + row
-      col = 0
-      row = ''
-    }
+  
+    data.text += '\n ' + options.footer
   }
 
-  if (col > 0) {
-    data.message += '\n' + row
-  }
-
-  data.message += '\n ' + CS_CONFIG.visual.footer
+  return new Command('cs', handler)
 }
